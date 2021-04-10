58f3d0106118a4723cd2d5ca2b9f02abd3feaffc
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-09 23:41:03 -0300

    create theme styles and vscode config

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+export default {
+  color: {
+    primaryColor: '#3BC6F1',
+    secondaryColor: '#36485B',
+    white: '#FFF',
+    black: '#000'
+  },
+  font: {
+    fontRegular: 'Segoe UI Regular',
+    fontBold: 'Segoe UI Bold',
+    fontItalic: 'Segoe UI Bold Italic'
+  }
+}
