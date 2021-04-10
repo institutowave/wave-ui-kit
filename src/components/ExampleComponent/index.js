e2d15cbe542e6469aeafc8d0789f8b17dd6b9786
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-09 22:39:23 -0300

    change init files to create-react-library

---
+++ src/components/ExampleComponent/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Test } from './styles'
+
+export const ExampleComponent = ({ text }) => {
+  return <Test>Example Component: {text}</Test>
+}
+
+export default ExampleComponent
--- src/components/ExampleComponent/styles.js
+++ src/components/ExampleComponent/styles.js
@@ -1,9 +1,9 @@
-/* add css module styles here (optional) */
+import styled from 'styled-components'
 
-.test {
+export const Test = styled.div`
   margin: 2em;
   padding: 0.5em;
   border: 2px solid #000;
   font-size: 2em;
   text-align: center;
-}
+`
